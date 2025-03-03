e7f2fc6d5b127fb0d3e9877eca294ffdc7cf26b8
JaylinCLeeSumlin <jaylin92603@gmail.com>
2025-03-03 17:45:15 -0500

    Update index.js such that it accounts for errors and/or and empty CSV file

--- assignment2-mod3/csv-reader/index.js
+++ assignment2-mod3/csv-reader/index.js
@@ -7,34 +7,39 @@ const Person = require("./person");
 // Read CSV file and return array of Person objects
 function loadCSV(filePath) {
     // Read data from CSV file
-    const data = fs.readFileSync(filePath, "utf8");
-    // console.log(`Data: ${data}`);
+    try {
+        const data = fs.readFileSync(filePath, "utf8");
+        // console.log(`Data: ${data}`);
+    
 
-    // Remove whitespace around data and split data into an array based on new line characters
-    const lines = data.trim().split("\n")
-    // console.log(`Lines: ${lines}`);
+        // Remove whitespace around data and split data into an array based on new line characters
+        const lines = data.trim().split("\n")
+        // console.log(`Lines: ${lines}`);
 
-    // First array element is split into a 2D array to defind CSV headers
-    const headers = lines[0].split(",");
-    // console.log(`Headers: ${headers}`)
+        // First array element is split into a 2D array to defind CSV headers
+        const headers = lines[0].split(",");
+        // console.log(`Headers: ${headers}`)
 
-    // Create an empty array called people
-    const people = [];
+        // Create an empty array called people
+        const people = [];
 
-    // Beginning at the 2nd elements in array lines...
-    for (let i = 1; i < lines.length; i++) {
-        // ... Split the current element into a 2D array...
-        const values = lines[i].split(",");
+        // Beginning at the 2nd elements in array lines...
+        for (let i = 1; i < lines.length; i++) {
+            // ... Split the current element into a 2D array...
+            const values = lines[i].split(",");
 
-        // .. Use the inner array's elements as values to create a Person object...
-        const person = new Person(values[0], values[1], values[2], values[3]);
+            // .. Use the inner array's elements as values to create a Person object...
+            const person = new Person(values[0], values[1], values[2], values[3]);
 
-        // .. and append the new Person object to array people
-        people.push(person);
+            // .. and append the new Person object to array people
+            people.push(person);
+        }
+
+        // Return the array of Person objects
+        return people;
+    } catch (err) {
+        console.log(`Error: ${err}`);
     }
-
-    // Return the array of Person objects
-    return people;
 }
 
 // Search array people by given name
@@ -74,8 +79,7 @@ function searchByPhone(people, searchPhone) {
     /*
     Return a new array of Person objects;
     - Iterate through array person
-    -
-    - Compare to determine a match
+    - Search phone value to determine if searchPhone is within the value
     */
     return people.filter(person => person.phone.includes(searchPhone));
 }
@@ -84,30 +88,35 @@ function searchByPhone(people, searchPhone) {
 const people = loadCSV("data.csv");
 // console.log(`People: ${people}`)
 
-// For each element in array person, call getDetails() function from Person class
-console.log("All people:");
-people.forEach(person => console.log(person.getDetails()));
+if (people.length == 0) {
+    console.log("Empty array")
+} else {
 
-// Call searchByName function and store resuts in searchNameResults
-const searchNameResults = searchByName(people, "Alice");
-// For each element in array person stored in searchNameResults, call getDetails() function
-console.log("\nSearch results for 'Alice':");
-searchNameResults.forEach(person => console.log(person.getDetails()));
+    // For each element in array person, call getDetails() function from Person class
+    console.log("All people:");
+    people.forEach(person => console.log(person.getDetails()));
 
-// Call searchByCity function and store resuts in searchCityResults
-const searchCityResults = searchByCity(people, "Chicago");
-// For each element in array person stored in searchCityResults, call getDetails() function
-console.log("\nSearch results for 'Chicago':");
-searchCityResults.forEach(person => console.log(person.getDetails()));
+    // Call searchByName function and store resuts in searchNameResults
+    const searchNameResults = searchByName(people, "Alice");
+    // For each element in array person stored in searchNameResults, call getDetails() function
+    console.log("\nSearch results for 'Alice':");
+    searchNameResults.forEach(person => console.log(person.getDetails()));
 
-// Call searchByAge function and store resuts in searchAgeResults
-const searchAgeResults = searchByAge(people, 20, 25);
-// For each element in array person stored in searchAgeResults, call getDetails() function
-console.log("\nSearch results for 20 to 25:");
-searchAgeResults.forEach(person => console.log(person.getDetails()));
+    // Call searchByCity function and store resuts in searchCityResults
+    const searchCityResults = searchByCity(people, "Chicago");
+    // For each element in array person stored in searchCityResults, call getDetails() function
+    console.log("\nSearch results for 'Chicago':");
+    searchCityResults.forEach(person => console.log(person.getDetails()));
 
-// Call searchByPhone function and store resuts in searchPhoneResults
-const searchPhoneResults = searchByPhone(people, "111");
-// For each element in array person stored in searchPhoneResults, call getDetails() function
-console.log("\nSearch results for '111':");
-searchPhoneResults.forEach(person => console.log(person.getDetails()));
+    // Call searchByAge function and store resuts in searchAgeResults
+    const searchAgeResults = searchByAge(people, 20, 25);
+    // For each element in array person stored in searchAgeResults, call getDetails() function
+    console.log("\nSearch results for 20 to 25:");
+    searchAgeResults.forEach(person => console.log(person.getDetails()));
+
+    // Call searchByPhone function and store resuts in searchPhoneResults
+    const searchPhoneResults = searchByPhone(people, "111");
+    // For each element in array person stored in searchPhoneResults, call getDetails() function
+    console.log("\nSearch results for '111':");
+    searchPhoneResults.forEach(person => console.log(person.getDetails()));
+}
