c9ed96298650e5f4096ac6356bc99ed42b82f874
JaylinCLeeSumlin <jaylin92603@gmail.com>
2025-03-30 14:19:54 -0400

    Complete project 1 of assignment3-mod3

--- assignment3-mod3/project1/js/ch08-proj01.js
+++ assignment3-mod3/project1/js/ch08-proj01.js
@@ -4,3 +4,22 @@ const shipping_threshold = prompt('Enter shipping threshold (1000)');
 
 /* add loop and other code here ... in this simple exercise we are not
    going to concern ourselves with minimizing globals, etc */
+// import { outputCartRow } from "./functions";
+// import { cart } from "./data";
+
+function displayRow() {
+   var subtotal = 0;
+   var total;
+   cart.forEach(item => {
+      // total = calculateTotal(item.quantity, item.product.price);
+      total = calculateTotal(item.quantity, item.product.price);
+      outputCartRow(item, total);
+      subtotal += total;
+});
+   subtotal = outputSubtotal(subtotal);
+   tax = outputTax(subtotal, tax_rate);
+   shipping = outputShipping(subtotal, shipping_threshold);
+   outputGrandTotal(subtotal, tax, shipping);
+}
+
+document.addEventListener("DOMContentLoaded", displayRow);
